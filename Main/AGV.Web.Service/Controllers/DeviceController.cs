9516d1a95b0837e87da9f3360e25b70dd4d5a8a2
Huang Yuan haung <dha834773759@163.com>
2019-07-02 17:14:24 +0800

    AgvInMisson to Order

---
+++ Main/AGV.Web.Service/Controllers/DeviceController.cs
@@ -0,0 +1,52 @@
+﻿using AGV.Web.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AGV.Web.Service.Controllers
+{
+    public class DeviceController : Controller
+    {
+
+
+        [HttpGet]
+        [HttpPost]
+        public JsonResult Init()
+        {
+
+            return Json(new { state = true });
+        }
+
+        [HttpGet]
+        [HttpPost]
+        public JsonResult LocationDevices(string id)
+        {
+            var query = Request.QueryString["action"];
+
+            if (Request.HttpMethod == "POST" && query == "arrived")
+            {
+                if (StaticData.SignalDict.ContainsKey(id))
+                {
+                    StaticData.SignalDict[id] = true;
+
+                }
+                return Json(new { state = true, id = id });
+            }
+            bool isArrived = false;
+            if (StaticData.SignalDict.ContainsKey(id))
+            {
+                isArrived = StaticData.SignalDict[id];
+            }
+            var node = new LocationDevice()
+            {
+                lastAction = query,
+                name = id,
+                lastActionStatus = isArrived ? ActionStatus.DONE : ActionStatus.EXECUTING,
+                status = isArrived ? Status.IDLE : Status.EXECUTING
+            };
+            return Json(node);
+        }
+    }
+}
---
+++ Main/AGV.Web.Service/Models/StaticData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Concurrent;
+namespace AGV.Web.Service.Models
+{
+    public class StaticData
+    {
+        public static BlockingCollection<WaitNode> WaitNodes = new BlockingCollection<WaitNode>();
+
+        public static ConcurrentDictionary<string, bool> SignalDict = new ConcurrentDictionary<string, bool>();
+
+        public static ConcurrentDictionary<string, List<ConfigNode>> ProductNodeDict = new ConcurrentDictionary<string, List<ConfigNode>>();
+    }
+}
